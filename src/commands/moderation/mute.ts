8cd1b8569dd65fe8415c6b689c4837e841f18028
Fish <conner.leuk@gmail.com>
2021-03-27 23:01:53 +0100

    now mute command :eyes:

---
+++ src/commands/moderation/mute.ts
@@ -0,0 +1,152 @@
+import {
+  Permissions,
+  PermissionOverwriteOption,
+  Collection,
+  MessageEmbed,
+  TextChannel,
+  Message,
+  BitFieldResolvable,
+} from "discord.js";
+import {
+  ApplicationCommandOptionType,
+  FishyCommandCode,
+  FishyCommandConfig,
+  FishyCommandHelp,
+} from "fishy-bot-framework/lib/types";
+import { ErrorEmbed } from "fishy-bot-framework/lib/utils/Embeds";
+import ms from "ms";
+
+export const mute_role_name = "Muted";
+const overwrites: PermissionOverwriteOption = {
+  SEND_MESSAGES: false,
+  ADD_REACTIONS: false,
+  SPEAK: false,
+  CREATE_INSTANT_INVITE: false,
+  ATTACH_FILES: false,
+  EMBED_LINKS: false,
+};
+
+let time_cache = new Collection<string, number>();
+const ttl = 12 * 60 * 60 * 1000;
+export const run: FishyCommandCode = async (client, interaction) => {
+  if (!interaction.guild) return;
+  if (!Object.values(interaction.mentions?.members || {})[0]) {
+    return interaction.send(new ErrorEmbed("Please enter a member to mute"));
+  }
+  const member = Object.values(interaction.mentions?.members || {})[0];
+  const user = Object.values(interaction.mentions?.users || {})[0];
+  const member_perms = new Permissions(Number.parseInt(member.permissions!));
+  if (member_perms.has("MANAGE_MESSAGES") || !user) {
+    return interaction.send(new ErrorEmbed("Unable to mute this member"));
+  }
+  let muterole = interaction.guild.roles.cache.find(
+    (role) => role.name === mute_role_name
+  )!;
+  if (!muterole) {
+    muterole = await interaction.guild.roles.create({
+      data: {
+        name: mute_role_name,
+        color: "#707070",
+      },
+      reason: "Used for muting people",
+    });
+  }
+  let new_member = await interaction.guild.members.fetch(user.id);
+  new_member.roles.add(muterole);
+
+  const time = interaction.args.find((arg) => arg.name == "time")?.value;
+  const miliseconds = time ? ms(time) : undefined;
+  const reason =
+    interaction.args.find((arg) => arg.name == "reason")?.value ||
+    "No reason provided";
+  const embed = new MessageEmbed();
+  embed.setTimestamp();
+  embed.setDescription(
+    `Muted: ${new_member}\nMuted by: ${interaction.member}\nTime: \`${
+      time && miliseconds && miliseconds > 10 ? ms(miliseconds) : "Indefinitely"
+    }\`\nReason: \`${reason}\``
+  );
+  embed.setTitle(`Succesfully muted member ${new_member.displayName}`);
+  embed.setFooter(`/un mute member: ${new_member.displayName}`);
+  interaction.send(embed)
+  if (time && miliseconds && miliseconds > 10) {
+    setTimeout(async () => {
+      try {
+        if (interaction.channel?.isText()) {
+          let newer_member = await interaction.guild!.members.fetch(
+            new_member.id
+          );
+          if (!newer_member.roles.cache.has(muterole.id)) {
+            return interaction.channel.send(
+              `Tried to unmute ${new_member}, but they already seem to be unmuted!`
+            );
+          }
+          await newer_member.roles.remove(muterole);
+          interaction.channel.send(
+            `${new_member} has been unmuted, after being muted for ${ms(
+              miliseconds
+            )} by ${interaction.member}`
+          );
+          // TODO: fix this
+          // interaction.channel.send()
+        }
+      } catch (err) {
+        if (interaction.channel?.isText()) {
+          interaction.channel.send(
+            new ErrorEmbed(
+              `Failed to unmute ${new_member.displayName}!`,
+              `Failed to unmute the member: ${new_member}, after being muted for ${ms(
+                miliseconds
+              )}`
+            )
+          );
+        }
+      }
+    }, miliseconds);
+  }
+
+  if (
+    !time_cache.has(interaction.guild.id) ||
+    time_cache.get(interaction.guild.id)! + ttl < Date.now()
+  ) {
+    time_cache.set(interaction.guild.id, Date.now());
+    interaction.guild.channels.cache.forEach(async (channel, id) => {
+      if (!channel.permissionsFor(muterole.id)) {
+        await channel.updateOverwrite(muterole.id, overwrites);
+      }
+    });
+  }
+};
+
+export const config: FishyCommandConfig = {
+  name: "mute",
+  bot_needed: true,
+  user_perms: ["MANAGE_MESSAGES"],
+  bot_perms: ["MANAGE_ROLES", "MANAGE_CHANNELS"],
+  interaction_options: {
+    name: "mute",
+    description: "Mute a member from this server",
+    options: [
+      {
+        name: "member",
+        description: "The member to mute",
+        type: ApplicationCommandOptionType.USER,
+        required: true,
+      },
+      {
+        name: "time",
+        description: "The time to mute someone",
+        type: ApplicationCommandOptionType.STRING,
+      },
+      {
+        name: "reason",
+        description: "The reason for muting someone",
+        type: ApplicationCommandOptionType.STRING,
+      },
+    ],
+  },
+};
+export const help: FishyCommandHelp = {
+  description: "Mute a member",
+  usage: "/mute member: member (time: number unit)",
+};
---
+++ src/commands/moderation/un.ts
@@ -0,0 +1,126 @@
+import { GuildMember, User } from "discord.js";
+import {
+  ApplicationCommandOptionType,
+  FishyCommandCode,
+  FishyCommandConfig,
+  FishyCommandHelp,
+} from "fishy-bot-framework/lib/types";
+import { ErrorEmbed } from "fishy-bot-framework/lib/utils/Embeds";
+import { mute_role_name } from "./mute";
+
+export const run: FishyCommandCode = async (client, interaction) => {
+  if (!interaction.guild) return;
+  console.log(interaction.raw_interaction);
+  const action = interaction.args[0].name;
+  if (!action || !interaction.args[0]) return;
+  if (action === "mute") {
+    if(!interaction.member?.hasPermission("MANAGE_MESSAGES")) return interaction.sendSilent("oi, nice try mate")
+    const toUnMuteRaw = Object.values(interaction.mentions?.users || {})[0];
+    const toUnMute = await interaction.guild?.members.fetch(
+      toUnMuteRaw?.id || ""
+    );
+    if (!toUnMuteRaw || !toUnMute) {
+      return interaction.send(new ErrorEmbed("Please enter somone to unmute"));
+    }
+    const member = interaction.member;
+    if (!member)
+      throw Error(
+        "SOMEHOW YOU DONT FUCKING EXIST OR SOMETHING IM TO F-ING TIRED FOR THIS"
+      );
+    let muterole = interaction.guild.roles.cache.find(
+      (role) => role.name === mute_role_name
+    )!;
+    if (!muterole) {
+      return new ErrorEmbed(
+        "Could not find the muted role",
+        `Couldnt find the mute role "${mute_role_name}"`
+      );
+    }
+
+    try {
+      if (!toUnMute.roles.cache.has(muterole.id)) {
+        return interaction.send(
+          `Tried to unmute ${toUnMute}, but they already seem to be unmuted!`
+        );
+      }
+      await toUnMute.roles.remove(muterole);
+      interaction.send(`${toUnMute} has been unmuted by ${member}`);
+    } catch (err) {
+      interaction.send(
+        new ErrorEmbed(
+          `Failed to unmute ${toUnMute.displayName}!`,
+          `Failed to unmute the member: ${toUnMute}`
+        )
+      );
+    }
+  } else if (action == "ban") {
+    const user_id = interaction.args[0].options!.find(
+      (arg) => arg.name == "user"
+    )?.value;
+    if (!user_id) {
+      return interaction.send(new ErrorEmbed("Please enter a user to un-ban"));
+    }
+    try {
+      if(!interaction.member?.hasPermission("BAN_MEMBERS")) return interaction.sendSilent("oi, nice try mate")
+      let guild_member = await interaction.guild!.members.ban(user_id);
+      let name = "";
+      if (guild_member instanceof GuildMember) name = guild_member.user.tag;
+      else if (guild_member instanceof User) name = guild_member.tag;
+      else name = guild_member;
+
+      interaction.send(
+        new ErrorEmbed(`Succesfully un-banned user "${name}"`).setColor("GREEN")
+      );
+    } catch (err) {
+      console.error(err);
+      interaction.send(
+        new ErrorEmbed(
+          "Could not un-ban that user",
+          `:(`
+        )
+      );
+    }
+  }
+};
+
+export const config: FishyCommandConfig = {
+  name: "un",
+  bot_needed: true,
+  bot_perms: ["MANAGE_ROLES", "BAN_MEMBERS"],
+  interaction_options: {
+    name: "un",
+    description: "Reverse previous actions",
+    options: [
+      {
+        name: "mute",
+        description: "Unmute a guild member",
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+        options: [
+          {
+            name: "member",
+            description: "The member to unmute",
+            type: ApplicationCommandOptionType.USER,
+            required: true,
+          },
+        ],
+      },
+      {
+        name: "ban",
+        description: "Un ban someone",
+        type: ApplicationCommandOptionType.SUB_COMMAND,
+        options: [
+          {
+            name: "user",
+            description: "The user to unban",
+            type: ApplicationCommandOptionType.USER,
+            required: true,
+          },
+        ],
+      },
+    ],
+  },
+};
+export const help: FishyCommandHelp = {
+  description: "Revers things! Ex: un mute, un ban, etc..",
+  usage: "/un [mute/ban]",
+};
