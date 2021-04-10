94d5d4034962112f864d224b31c175a411b2f6c6
Fish <conner.leuk@gmail.com>
2021-04-11 00:08:44 +0200

    fix pls

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ require("dotenv").config({});
 let Client = new FishyBot.FishyClient({
   author: "Fish#2455",
   token: process.env.TOKEN!,
-  cmd_dir: "./lib/commands",
-  event_dir: "./lib/events",
+  cmd_dir: "lib/commands",
+  event_dir: "lib/events",
   db_uri: process.env.DB_URI!,
   guild_model: GuildModel,
   disable_msg_notfound: true,
