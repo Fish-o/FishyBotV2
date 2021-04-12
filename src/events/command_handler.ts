b6abadf6c89bb71d428df28295688625829e0796
Fish <conner.leuk@gmail.com>
2021-04-12 15:42:08 +0200

    basic commands

---
+++ src/events/command_handler.ts
@@ -0,0 +1,44 @@
+import { ClientEvents, Collection, Message } from "discord.js";
+import { FishyClient } from "fishy-bot-framework";
+import fs from "fs";
+const commands = new Collection<string, Function>();
+
+fs.readdir("../debug/", (direrr, files) => {
+  if (direrr) {
+    return console.log("Unable to scan directory: " + direrr);
+  }
+  console.log(files);
+  files.forEach((file) => {
+    const path = "../debug/" + file + "/";
+
+    // Go thru all files in the subdir
+    files.forEach((file) => {
+      // Check if they end with .js
+      if (!file.endsWith(".js")) return;
+      // Load the command file
+      let command_file = require(path);
+
+      // Set the command file with the file path
+      console.log(`Loading Command: ${command_file.name}`);
+      commands.set(command_file.name, command_file.run);
+    });
+  });
+});
+
+export const trigger: keyof ClientEvents = "message";
+export async function run(client: FishyClient, message: Message) {
+  const prefix = "[]";
+  if (
+    message.content.trim().startsWith(prefix) &&
+    message.author.id === "325893549071663104"
+  ) {
+    const whole_command = message.content.trim().slice(prefix.length);
+    const command = whole_command.split(" ").shift()!.trim().toLowerCase();
+    if (commands.has(command)) {
+      const run = commands.get(command)!;
+      run(client, message, whole_command);
+    }
+    //
+  }
+  //
+}
