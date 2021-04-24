6e56c264ea8ce1d389bdcf7efb16f4eed1f1b487
Fish <conner.leuk@gmail.com>
2021-04-24 15:56:51 +0200

    started working on utils command

---
+++ src/commands/admin/utils.ts
@@ -0,0 +1,494 @@
+import {
+  ColorResolvable,
+  Message,
+  MessageEmbed,
+  Role,
+  RoleData,
+  Snowflake,
+} from "discord.js";
+import {
+  ApplicationCommandOptionType,
+  FishyCommandCode,
+  FishyCommandConfig,
+  FishyCommandHelp,
+} from "fishy-bot-framework/lib/types";
+import { ErrorEmbed } from "fishy-bot-framework/lib/utils/Embeds";
+import ms from "ms";
+
+export const run: FishyCommandCode = async (client, interaction) => {
+  if (!interaction.guild) return;
+  const main_util = interaction.data.options[0];
+  const main_util_name = main_util?.name;
+  if (main_util_name === "role") {
+    const sub_util = main_util.options[0];
+    const sub_util_name = sub_util?.name;
+    if (sub_util_name === "delete") {
+      interaction.send("Deleting roles...");
+      const deleted_roles: Array<string> = [];
+      const failed_roles: Array<string> = [];
+
+      const enteredRole = interaction?.mentions?.roles?.first();
+      const stringRoles = sub_util.options.find((opt) => opt.name === "roles")
+        ?.value;
+      if (enteredRole) {
+        const role = await interaction.guild.roles.fetch(enteredRole.id);
+        if (role) {
+          try {
+            await role.delete(
+              `Delete command called by: ${interaction.member?.user.tag}`
+            );
+            deleted_roles.push(enteredRole.name);
+          } catch (err) {
+            failed_roles.push(enteredRole.name);
+          }
+        } else {
+          failed_roles.push(enteredRole.name);
+        }
+      } else if (stringRoles && typeof stringRoles === "string") {
+        const res = [...stringRoles.matchAll(/(\d{18})/g)];
+        await Promise.all(
+          res.map(async (array) => {
+            const role_id = array[array.length - 1];
+            const role = await interaction.guild!.roles.fetch(role_id);
+            if (role) {
+              try {
+                const name = role.name.toString();
+                await role.delete();
+                deleted_roles.push(name);
+              } catch (err) {
+                failed_roles.push(role.name);
+              }
+            } else {
+              failed_roles.push(role_id);
+            }
+            return;
+          })
+        );
+      } else {
+        return interaction.edit(
+          new ErrorEmbed(
+            "Failed to delete roles",
+            "You must provide a role or roles to delete"
+          )
+        );
+      }
+      const embed = new MessageEmbed().setTimestamp();
+      if (deleted_roles[0] && !failed_roles[0]) {
+        embed.setTitle("Successfully deleted all roles").setColor("GREEN");
+        embed.setDescription(
+          `Deleted roles:\n\`${deleted_roles.join("`, `")}\`\n`
+        );
+      } else if (deleted_roles[0] && failed_roles[0]) {
+        embed.setTitle("Successfully deleted some roles").setColor("YELLOW");
+        embed.setDescription(
+          `Deleted roles:\n\`${deleted_roles.join("`, `")}\`\n`
+        );
+        embed.setDescription(
+          `Failed roles:\n\`${failed_roles.join("`, `")}\`\n`
+        );
+      } else if (!deleted_roles[0] && failed_roles[0]) {
+        embed.setTitle("Failed to delete roles").setColor("RED");
+        embed.setDescription(
+          `Failed roles:\n\`${failed_roles.join("`, `")}\`\n`
+        );
+      }
+      interaction.edit(embed);
+    } else if (sub_util_name === "create") {
+      const name = sub_util.options.find((opt) => opt.name === "name")?.value;
+      const color = sub_util.options.find((opt) => opt.name === "color")?.value;
+      const raw = sub_util.options.find((opt) => opt.name === "raw")?.value;
+      if ((name || color) && !raw) {
+        const obj: RoleData = {};
+        if (typeof name === "string") {
+          obj.name = name;
+        }
+        if (typeof color === "string") {
+          obj.color = colourNameToHex(color) || color;
+        }
+
+        const role = await interaction.guild.roles.create({
+          data: obj,
+          reason: `'/u role create' ran by: ${interaction.member?.user.tag}`,
+        });
+        const embed = new MessageEmbed()
+          .setTitle("Successfully created a role!")
+          .setColor("GREEN")
+          .setTimestamp()
+          .setDescription(`Created role: ${role}`);
+        interaction.send(embed);
+      } else if (raw && typeof raw === "string") {
+        const rawRoles = raw.split(";");
+        const failed: Array<string> = [];
+        const created: Array<Role> = [];
+        await Promise.all(
+          rawRoles.map(async (rawRole) => {
+            if (rawRole.trim() == "") return;
+            rawRole = rawRole.trim();
+            const splitRole = rawRole.split("|");
+            if (!splitRole[0]) return failed.push(`1: \`${rawRole}\``);
+            const obj: RoleData = {};
+            obj.name = splitRole[0];
+            if (splitRole[1]) {
+              obj.color = colourNameToHex(splitRole[1]) || splitRole[1];
+            }
+
+            try {
+              const role = await interaction.guild?.roles.create({
+                data: obj,
+                reason: `'/u role create' ran by: ${interaction.member?.user.tag}`,
+              });
+              if (role) created.push(role);
+              else failed.push(`2: \`${rawRole}\``);
+            } catch (err) {
+              failed.push(`3: \`${rawRole}\``);
+            }
+            return;
+          })
+        );
+        const embed = new MessageEmbed();
+        if (!failed[0] && created[0]) {
+          embed
+            .setTitle("Successfully created all roles!")
+            .setColor("GREEN")
+            .setTimestamp()
+            .setDescription(`Created roles: \n${created.join(", ")}`);
+        } else if (failed[0] && created[0]) {
+          embed
+            .setTitle("Successfully created some roles")
+            .setColor("YELLOW")
+            .setTimestamp()
+            .setDescription(
+              `Created roles: \n${created.join(
+                ", "
+              )}\n\nFailed roles: \n${failed.join(",\n")}`
+            );
+        } else if (failed[0] && !created[0]) {
+          embed
+            .setTitle("Failed to create any role")
+            .setColor("RED")
+            .setTimestamp()
+            .setDescription(`Failed roles: \n${failed.join(",\n")}`);
+        }
+        interaction.send(embed);
+      }
+    } else if (sub_util_name === "clone") {
+      const origin = interaction.data.mentions?.roles?.first();
+      const name = sub_util.options.find((opt) => opt.name === "name")?.value;
+      const color = sub_util.options.find((opt) => opt.name === "color")?.value;
+      const assign = sub_util.options.find((opt) => opt.name === "assign")
+        ?.value;
+      if (!origin)
+        return interaction.send(
+          new ErrorEmbed(
+            "Failed to clone role",
+            "You must provide a role to clone"
+          )
+        );
+      const data: RoleData = {};
+      if (typeof name === "string") data.name = name || `${origin.name} (copy)`;
+      else data.name = `${origin.name} (copy)`;
+      if (typeof color === "string")
+        data.color = colourNameToHex(color) || color || origin.color;
+      else data.color = origin.color;
+      data.permissions = Number.parseInt(origin.permissions);
+      data.mentionable = origin.mentionable;
+      data.hoist = origin.hoist;
+      data.position = origin.position;
+      try {
+        let role = await interaction.guild.roles.create({
+          data: data,
+          reason: `'/u role create' ran by: ${interaction.member?.user.tag}`,
+        });
+        if (!role) {
+          delete data.position;
+          role = await interaction.guild.roles.create({
+            data: data,
+            reason: `'/u role create' ran by: ${interaction.member?.user.tag}`,
+          });
+        }
+        if (!role)
+          return interaction.send(new ErrorEmbed("Failed to clone role"));
+        const embed = new MessageEmbed()
+          .setTitle("Successfully cloned the role!")
+          .setTimestamp()
+          .setColor("GREEN");
+        let desc = `Successfully made the new role "${role}"\n`;
+
+        if (typeof assign == "boolean" && assign === true) {
+          const originRole = await interaction.guild.roles.fetch(origin.id)!;
+          const members = originRole!.members;
+          if (members.size > 2000) {
+            desc += `**Didn't assign roles as it would take over ${ms(
+              members.size * 205
+            )}**`;
+          } else {
+            members.keyArray().forEach((member_id, index) => {
+              setTimeout(() => {
+                members.get(member_id)?.roles.add(role);
+              }, index * 205);
+            });
+
+            desc += `Started applying the roles to server members, this can take up to ${ms(
+              205 * members.size
+            )}`;
+          }
+
+          //
+        }
+        embed.setDescription(desc);
+        interaction.send(embed);
+      } catch (err) {
+        interaction.send(new ErrorEmbed("Couldn't clone the role: ", err));
+      }
+    }
+  }
+};
+
+export const config: FishyCommandConfig = {
+  name: "u",
+  bot_needed: true,
+  bot_perms: ["MANAGE_ROLES"],
+  user_perms: ["MANAGE_ROLES"],
+  interaction_options: {
+    name: "u",
+    description:
+      "Utility commands, basicity stuff you can already do in discord, but this makes it easier!",
+    options: [
+      {
+        name: "role",
+        description: "Commands related to roles",
+        type: ApplicationCommandOptionType.SUB_COMMAND_GROUP,
+        options: [
+          {
+            name: "delete",
+            description: "Delete a single, or multiple roles",
+            type: ApplicationCommandOptionType.SUB_COMMAND,
+            options: [
+              {
+                name: "role",
+                description: "Delete a single role",
+                type: ApplicationCommandOptionType.ROLE,
+              },
+              {
+                name: "roles",
+                description: "Delete multiple roles",
+                type: ApplicationCommandOptionType.STRING,
+              },
+            ],
+          },
+          {
+            name: "create",
+            description: "Creates roles",
+            type: ApplicationCommandOptionType.SUB_COMMAND,
+            options: [
+              {
+                name: "name",
+                description: "The name of the new role",
+                type: ApplicationCommandOptionType.STRING,
+              },
+              {
+                name: "color",
+                description: "The color of the new role",
+                type: ApplicationCommandOptionType.STRING,
+              },
+              {
+                name: "raw",
+                description:
+                  "(advanced) name|color (optional);. Examples: admin|yellow; mod|blue",
+                type: ApplicationCommandOptionType.STRING,
+              },
+            ],
+          },
+          {
+            name: "clone",
+            description: "Clone a role",
+            type: ApplicationCommandOptionType.SUB_COMMAND,
+            options: [
+              {
+                name: "origin",
+                description: "The role to clone",
+                type: ApplicationCommandOptionType.ROLE,
+                required: true,
+              },
+              {
+                name: "name",
+                description: "The name of the new role",
+                type: ApplicationCommandOptionType.STRING,
+                required: true,
+              },
+              {
+                name: "color",
+                description: "The color of the new role",
+                type: ApplicationCommandOptionType.STRING,
+              },
+              {
+                name: "assign",
+                description:
+                  "Assign the new role to the people who have the clone",
+                type: ApplicationCommandOptionType.BOOLEAN,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
+export const help: FishyCommandHelp = {
+  description: "u",
+  usage: "/u [util]",
+};
+
+function colourNameToHex(colour: string): ColorResolvable | undefined {
+  const colours = {
+    aliceblue: "#f0f8ff",
+    antiquewhite: "#faebd7",
+    aqua: "#00ffff",
+    aquamarine: "#7fffd4",
+    azure: "#f0ffff",
+    beige: "#f5f5dc",
+    bisque: "#ffe4c4",
+    black: "#000000",
+    blanchedalmond: "#ffebcd",
+    blue: "#0000ff",
+    blueviolet: "#8a2be2",
+    brown: "#a52a2a",
+    burlywood: "#deb887",
+    cadetblue: "#5f9ea0",
+    chartreuse: "#7fff00",
+    chocolate: "#d2691e",
+    coral: "#ff7f50",
+    cornflowerblue: "#6495ed",
+    cornsilk: "#fff8dc",
+    crimson: "#dc143c",
+    cyan: "#00ffff",
+    darkblue: "#00008b",
+    darkcyan: "#008b8b",
+    darkgoldenrod: "#b8860b",
+    darkgray: "#a9a9a9",
+    darkgreen: "#006400",
+    darkkhaki: "#bdb76b",
+    darkmagenta: "#8b008b",
+    darkolivegreen: "#556b2f",
+    darkorange: "#ff8c00",
+    darkorchid: "#9932cc",
+    darkred: "#8b0000",
+    darksalmon: "#e9967a",
+    darkseagreen: "#8fbc8f",
+    darkslateblue: "#483d8b",
+    darkslategray: "#2f4f4f",
+    darkturquoise: "#00ced1",
+    darkviolet: "#9400d3",
+    deeppink: "#ff1493",
+    deepskyblue: "#00bfff",
+    dimgray: "#696969",
+    dodgerblue: "#1e90ff",
+    firebrick: "#b22222",
+    floralwhite: "#fffaf0",
+    forestgreen: "#228b22",
+    fuchsia: "#ff00ff",
+    gainsboro: "#dcdcdc",
+    ghostwhite: "#f8f8ff",
+    gold: "#ffd700",
+    goldenrod: "#daa520",
+    gray: "#808080",
+    green: "#008000",
+    greenyellow: "#adff2f",
+    honeydew: "#f0fff0",
+    hotpink: "#ff69b4",
+    indianred: "#cd5c5c",
+    indigo: "#4b0082",
+    ivory: "#fffff0",
+    khaki: "#f0e68c",
+    lavender: "#e6e6fa",
+    lavenderblush: "#fff0f5",
+    lawngreen: "#7cfc00",
+    lemonchiffon: "#fffacd",
+    lightblue: "#add8e6",
+    lightcoral: "#f08080",
+    lightcyan: "#e0ffff",
+    lightgoldenrodyellow: "#fafad2",
+    lightgrey: "#d3d3d3",
+    lightgreen: "#90ee90",
+    lightpink: "#ffb6c1",
+    lightsalmon: "#ffa07a",
+    lightseagreen: "#20b2aa",
+    lightskyblue: "#87cefa",
+    lightslategray: "#778899",
+    lightsteelblue: "#b0c4de",
+    lightyellow: "#ffffe0",
+    lime: "#00ff00",
+    limegreen: "#32cd32",
+    linen: "#faf0e6",
+    magenta: "#ff00ff",
+    maroon: "#800000",
+    mediumaquamarine: "#66cdaa",
+    mediumblue: "#0000cd",
+    mediumorchid: "#ba55d3",
+    mediumpurple: "#9370d8",
+    mediumseagreen: "#3cb371",
+    mediumslateblue: "#7b68ee",
+    mediumspringgreen: "#00fa9a",
+    mediumturquoise: "#48d1cc",
+    mediumvioletred: "#c71585",
+    midnightblue: "#191970",
+    mintcream: "#f5fffa",
+    mistyrose: "#ffe4e1",
+    moccasin: "#ffe4b5",
+    navajowhite: "#ffdead",
+    navy: "#000080",
+    oldlace: "#fdf5e6",
+    olive: "#808000",
+    olivedrab: "#6b8e23",
+    orange: "#ffa500",
+    orangered: "#ff4500",
+    orchid: "#da70d6",
+    palegoldenrod: "#eee8aa",
+    palegreen: "#98fb98",
+    paleturquoise: "#afeeee",
+    palevioletred: "#d87093",
+    papayawhip: "#ffefd5",
+    peachpuff: "#ffdab9",
+    peru: "#cd853f",
+    pink: "#ffc0cb",
+    plum: "#dda0dd",
+    powderblue: "#b0e0e6",
+    purple: "#800080",
+    rebeccapurple: "#663399",
+    red: "#ff0000",
+    rosybrown: "#bc8f8f",
+    royalblue: "#4169e1",
+    saddlebrown: "#8b4513",
+    salmon: "#fa8072",
+    sandybrown: "#f4a460",
+    seagreen: "#2e8b57",
+    seashell: "#fff5ee",
+    sienna: "#a0522d",
+    silver: "#c0c0c0",
+    skyblue: "#87ceeb",
+    slateblue: "#6a5acd",
+    slategray: "#708090",
+    snow: "#fffafa",
+    springgreen: "#00ff7f",
+    steelblue: "#4682b4",
+    tan: "#d2b48c",
+    teal: "#008080",
+    thistle: "#d8bfd8",
+    tomato: "#ff6347",
+    turquoise: "#40e0d0",
+    violet: "#ee82ee",
+    wheat: "#f5deb3",
+    white: "#ffffff",
+    whitesmoke: "#f5f5f5",
+    yellow: "#ffff00",
+    yellowgreen: "#9acd32",
+  };
+
+  if (Object.keys(colours).includes(colour.toLowerCase().split(" ").join(""))) {
+    return Object.values(colours)[
+      Object.keys(colours).indexOf(colour.toLowerCase().split(" ").join(""))
+    ];
+  }
+  if (/^#[0-9A-F]{6}$/i.test(colour)) return colour;
+  return undefined;
+}
