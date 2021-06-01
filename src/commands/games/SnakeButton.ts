27315062ee0c580c75ece6fa7c35d48690126e93
Fish <conner.leuk@gmail.com>
2021-06-01 16:47:25 +0200

    snakeButton

---
+++ src/commands/games/SnakeButton.ts
@@ -0,0 +1,20 @@
+import {
+  FishyButtonCommandCode,
+  FishyButtonCommandConfig,
+} from "fishy-bot-framework/lib/types";
+import { PressButton } from "./Snake";
+
+export const run: FishyButtonCommandCode = async (client, interaction) => {
+  interaction.buttonAck();
+  const custom_id = interaction.customID;
+  const data = custom_id.slice(config.custom_id.length).split("|");
+  const button = data[0];
+  PressButton(interaction.raw_user.id, button);
+};
+
+export const config: FishyButtonCommandConfig = {
+  custom_id: "snake_",
+  user_perms: [],
+  atStart: true,
+  bot_needed: false,
+};
