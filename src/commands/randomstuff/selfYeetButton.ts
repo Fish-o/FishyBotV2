a2b2a1c97f4ff53c39ee2bf5ef2161b9e20f1c24
Fish <64684124+Fish-o@users.noreply.github.com>
2022-01-24 08:54:35 +0100

    Update selfYeetButton.ts

--- src/commands/randomstuff/selfYeetButton.ts
+++ src/commands/randomstuff/selfYeetButton.ts
@@ -7,39 +7,7 @@ import {
 import { ErrorEmbed } from "fishy-bot-framework/lib/utils/Embeds";
 
 export const run: FishyComponentCommandCode = async (client, interaction) => {
-  if (Math.random() > 0.02 || !interaction.member?.kickable) {
-    interaction.send(`${interaction.member} pressed me :(`);
-  } else {
-    try {
-      const invite =
-        (await interaction.guild?.fetchInvites())?.find(
-          (inv) =>
-            !inv.expiresTimestamp ||
-            inv.expiresTimestamp >= Date.now() + 2 * 24 * 60 * 60 * 1000
-        ) ||
-        (await interaction.channel?.createInvite({
-          maxAge: 0,
-          maxUses: 0,
-        }));
-      if (!invite?.url) {
-        return interaction.send(`${interaction.member} pressed me :(`);
-      }
-
-      await interaction.member.user.send(
-        `Don't press me again >:(\n${invite.url}`
-      );
-
-      await interaction.member.kick(
-        `roles: '${interaction.member.roles.cache
-          .map((role) => role.name)
-          .join("', '")}'`
-      );
-      interaction.send("THAT WHAT YOU GET FOR PRESSING ME! ");
-    } catch (err) {
-      interaction.send(`${interaction.member} pressed me :(`);
-      console.log(err);
-    }
-  }
+  interaction.send(`${interaction.member} pressed me :(`);
 };
 
 export const config: FishyComponentCommandConfig = {
